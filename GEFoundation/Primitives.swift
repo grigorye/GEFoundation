f1b1937c52f3f08432b27fae3b0a46b50fd35083
Grigory Entin <grigory.entin@gmail.com>
2016-10-23 21:53:35 +0200

    Moved Primitives to GEFoundation.

---
+++ GEFoundation/Primitives.swift
@@ -0,0 +1,13 @@
+//
+//  Primitives.swift
+//  GEBase
+//
+//  Created by Grigory Entin on 18.07.15.
+//  Copyright © 2015 Grigory Entin. All rights reserved.
+//
+
+public var _1 = true
+public var _0 = false
+
+public typealias Handler = () -> Void
+
